51d228a3336173c93d47a5e0221a4783fabdb001
demondandy <okorodandyj@gmail.com>
2022-03-10 15:54:18 -0800

    Low level programming with C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /*
  * main - main blocK
  * Return: Always 0 (Success)
  *
  */
-
-
-int main(void) /* main block */
+int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
