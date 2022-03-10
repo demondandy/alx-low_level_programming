461a3757eb5bd599e2d87f2864208e2ae49b3a90
demondandy <okorodandyj@gmail.com>
2022-03-10 09:46:47 -0800

    Low level programming with C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,18 @@
 
 /**
  *
- *  Programmed by demondandy
+ *  * main - Entry point
  *
- *  C Program To Return: 0
+ *   *
  *
- *    */
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
-
+	
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	return (0);
 }
