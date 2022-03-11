cbbd783adff3cf8d67096a6a86386c5571ab9fca
demondandy <okorodandyj@gmail.com>
2022-03-10 17:54:53 -0800

    Low level programming with C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - Entry Point.
  *
- *  Programmed By demondandy
- *
- *  Program Returns: 0
- *
- *    */
-
+ * Return: Always 0 (Sucess).
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
 }
