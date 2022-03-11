3201e463e012fff1c82ae2b98344159d08833798
demondandy <okorodandyj@gmail.com>
2022-03-10 17:33:20 -0800

    Low level programming with C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,9 @@
 
 /*
  * main - Entry point.
+ *
+ * Description: prints Programming is like building a multilingual puzzle.
+ *
  * Return: 0 if exited properly, non-zero otherwise.
  */
 int main(void)
