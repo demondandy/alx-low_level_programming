c1d04f7f5d6537499619b526a311b491ac73f083
demondandy <okorodandyj@gmail.com>
2022-03-11 07:45:45 -0800

    Added task 0 to 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program will assign a random number to the variable n  
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negavtive\n", n);
+
+	return (0);
+}
