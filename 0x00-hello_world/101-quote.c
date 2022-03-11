644f783e6af4cabaaf596140a3e91393e5685cbd
demondandy <okorodandyj@gmail.com>
2022-03-10 17:21:05 -0800

    Low level programming with C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
  * main - Entry point.
@@ -9,7 +11,12 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	write(2,
+
+				      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+
+				      	      s);
 	return (1);
 }
 
