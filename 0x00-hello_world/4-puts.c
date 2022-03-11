8277d523de719d108daac5824c2685b07058e9a5
demondandy <okorodandyj@gmail.com>
2022-03-10 16:23:42 -0800

    Low level programming with C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
 /*
+ * main - print the phrase "Programming is like building a multilingual puzzle
  *
- * main - print the string in the put function
- *
- * Description: using the main function
- *
- * 
- * Return: 0
  */
 int main(void)
 {
